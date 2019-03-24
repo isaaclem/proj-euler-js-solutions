954293c4986a42b5c3600745da861adfee46a676
isaaclem <lem.chiew.choon@gmail.com>
2019-03-24 20:57:04 +0800

    Enhanced Solution 1

--- problem_1.js
+++ problem_1.js
@@ -11,3 +11,17 @@ for (let n = 1; n < 1000; n++) {
 }
 
 console.log(result);
+
+//Enhanced solution:
+//3+6+9+12+......+999 = 3*(1+2+3+4+...+333)
+//5+10+15+...+995 = 5*(1+2+....+199)
+
+// 1+2+3+...+p= p*(p+1) / 2
+const target = 999;
+
+const SumDivisibleBy = n => {
+  let p = Math.floor(target / n);
+  return (n * (p * (p + 1))) / 2;
+};
+
+console.log(SumDivisibleBy(3) + SumDivisibleBy(5) - SumDivisibleBy(15));
